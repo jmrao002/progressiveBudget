2e12a6607ab212e9dfc4665c04a52afe8fa81778
Michael Rao <jmichaelrao@gmail.com>
2021-08-14 18:02:16 -0500

    add manifest, service worker, and indexedDB files

---
+++ public/service-worker.js
@@ -0,0 +1,10 @@
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/manifest.webmanifest",
+  "/styles.css",
+  "/index.js",
+  "/indexDB.js",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+];
